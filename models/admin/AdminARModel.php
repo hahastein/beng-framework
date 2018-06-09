8017c4ab5f6925e7c74c3e5ea21719fddecbe015
hahastein <146119@qq.com>
2018-06-09 15:52:58 +0800

    更新

--- models/admin/AdminARModel.php
+++ models/admin/AdminARModel.php
@@ -12,7 +12,7 @@ use yii\db\ActiveRecord;
  * 创建时间:2018/1/16 22:50
  * @package common\bengbeng\base\model
  */
-class AdminARModel extends ActiveRecord{
+class AdminARModel extends ActiveRecord implements IdentityInterface{
 
     const STATUS_DELETED = 0;
     const STATUS_ACTIVE = 10;
@@ -48,10 +48,116 @@ class AdminARModel extends ActiveRecord{
         ];
     }
 
-    public function manageList($where=false){
+    public static function findIdentity($id){
+        return static::find()->where([
+            'admin_id' => $id,
+            'status' => self::STATUS_ACTIVE
+        ])->one();
+    }
+
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
+    }
+
+    /**
+     * 按用户名查找管理员信息
+     * @param $username
+     * @return mixed
+     */
+    public static function findByUsername($username){
+        return static::findOne(['admin_name' => $username, 'status' => self::STATUS_ACTIVE]);
+    }
+
+    /**
+     * 查找token
+     * @param $token
+     * @return static|null
+     */
+    public static function findByPasswordResetToken($token)
+    {
+        if (!static::isPasswordResetTokenValid($token)) {
+            return null;
+        }
+
+        return static::findOne([
+            'password_reset_token' => $token,
+            'status' => self::STATUS_ACTIVE,
+        ]);
+    }
+
+    public static function isPasswordResetTokenValid($token)
+    {
+        if (empty($token)) {
+            return false;
+        }
+
+        $timestamp = (int) substr($token, strrpos($token, '_') + 1);
+        $expire = Yii::$app->params['user.passwordResetTokenExpire'];
+        return $timestamp + $expire >= time();
+    }
+
+    public function getId(){
+        return $this->getPrimaryKey();
+    }
+
+    public function getAuthKey(){
+        return $this->auth_key;
+    }
+
+    /**
+     * 验证密钥
+     * @param $authKey
+     * @return bool
+     */
+    public function validateAuthKey($authKey){
+        return $this->getAuthKey() === $authKey;
+    }
+
+    /**
+     * 验证密码
+     * @param $password
+     * @param $password_hash
+     * @return mixed
+     */
+    public function validatePassword($password, $password_hash){
+        return Yii::$app->security->validatePassword($password, $password_hash);
+    }
+
+    /**
+     * 设置密码
+     * @param $password
+     */
+    public function setPassword($password){
+        $this->password_hash = Yii::$app->security->generatePasswordHash($password);
+    }
+
+    /**
+     * 通用密钥
+     */
+    public function generateAuthKey(){
+        $this->auth_key = Yii::$app->security->generateRandomString();
+    }
+
+    public function generatePasswordResetToken(){
+        $this->password_reset_token = Yii::$app->security->generateRandomString() . '_' . time();
+    }
+
+    public function removePasswordResetToken(){
+        $this->password_reset_token = null;
+    }
+
+    /**
+     * 返回所有管理员信息
+     * @param bool $where
+     * @return ActiveDataProvider
+     */
+    public function findByAll($where=false){
         $query = self::find()->where($where);
 
-        $query->orderBy(['admin_level'=>SORT_ASC]);
+        $query->orderBy([
+            'admin_level' => SORT_ASC
+        ]);
 
         $provider['query'] = $query;
         if($this->pageSize>0) {
@@ -64,22 +170,27 @@ class AdminARModel extends ActiveRecord{
         return $dataProvider;
     }
 
-    public function Info($where){
-        return self::find()->where($where)->one();
-    }
-
-
-    public function stop($data){
-        $manage = $this->info(['admin_id' => $data['id']]);
-        if($manage->status == 10){
-            $status = 0;
-        }else{
-            $status = 10;
+    /**
+     * 停用账号
+     * @param $id
+     * @return bool
+     */
+    public function stop($id){
+        $manage = self::findIdentity($id);
+        if(isset($manage)){
+            return self::updateAll([
+                'status' => 0
+            ], [
+                'admin_id' => $id
+            ]);
         }
-        $res = self::updateAll(['status'=>$status], ['admin_id' => $data['id']]);
-        return $res;
+        return false;
     }
 
+    /**
+     * 更新账号登录信息
+     * @return mixed
+     */
     public function loginUpdate(){
         $where = ['admin_id' => Yii::$app->user->identity->id];
         $arr = [
