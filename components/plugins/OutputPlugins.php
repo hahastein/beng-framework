b7a60b8a446994d632af1016c1459b6984213d82
hahastein <146119@qq.com>
2018-06-13 18:46:37 +0800

    更新

---
+++ components/plugins/OutputPlugins.php
@@ -0,0 +1,9 @@
+<?php
+namespace bengbeng\framework\components\plugins;
+
+class OutputPlugins{
+
+    public function outHtml($arr = []){
+        echo '<meta charset="UTF-8"><pre style="display: block;padding: 9.5px;margin: 40px 0px 10px 0px;font-size: 13px;line-height: 1.42857;color: #333;word-break: break-all;word-wrap: break-word;background-color: #F5F5F5;border: 1px solid #CCC;border-radius: 4px;">'.print_r($arr,true).'</pre>';
+    }
+}
