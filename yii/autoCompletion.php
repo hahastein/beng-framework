3a10d6e0f42f5d70009741da8372dad181f57a6c
hahastein <146119@qq.com>
2018-06-14 14:41:03 +0800

    更新

--- yii/autoCompletion.php
+++ yii/autoCompletion.php
@@ -1,6 +1,18 @@
 <?php
 
 /**
+ * Yii bootstrap file.
+ * Used for enhanced IDE code autoCompletion.
+ */
+class Yii extends \yii\BaseYii
+{
+    /**
+     * @var autoCompletion the application instance
+     */
+    public static $app;
+}
+
+/**
  * @property \bengbeng\framework\components\plugins\OutputPlugins $B B is plugins
  */
 abstract class autoCompletion extends yii\base\Application
