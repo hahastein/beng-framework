bcdec04bb1049e73df88c06982c530f7bc82c0f1
hahastein <146119@qq.com>
2018-06-14 14:08:07 +0800

    更新

--- components/plugins/OutputPlugins.php
+++ components/plugins/OutputPlugins.php
@@ -10,11 +10,13 @@ class OutputPlugins extends Component{
     /**
      * 输出测试数据到页面
      * @param string $output_content
+     * @throws \yii\base\ExitException
      */
     public function outHtml($output_content = self::DEFAULT_OUTPUT_CONTENT){
+        \Yii::$app->response->headers->add("Content-type","text/html;charset=utf-8");
         \Yii::$app->response->format = \yii\web\Response::FORMAT_HTML;
-        $content = '<meta charset="UTF-8"><pre style="display: block;padding: 9.5px;margin: 40px 0px 10px 0px;font-size: 13px;line-height: 1.42857;color: #333;word-break: break-all;word-wrap: break-word;background-color: #F5F5F5;border: 1px solid #CCC;border-radius: 4px;">'.print_r($output_content,true).'</pre>';
+        $content = '<pre style="display: block;padding: 9.5px;margin: 40px 0px 10px 0px;font-size: 13px;line-height: 1.42857;color: #333;word-break: break-all;word-wrap: break-word;background-color: #F5F5F5;border: 1px solid #CCC;border-radius: 4px;">'.print_r($output_content,true).'</pre>';
         \Yii::$app->response->content = $content;
-//        exit();
+        \Yii::$app->end();
     }
 }
