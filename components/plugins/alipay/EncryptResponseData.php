aca90294957cc4c53090e5e19791749abc4dddef
hahastein <146119@qq.com>
2018-06-15 02:19:57 +0800

    update user handle

---
+++ components/plugins/alipay/EncryptResponseData.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *  TODO 补充说明 
+ *
+ * User: jiehua
+ * Date: 16/3/30
+ * Time: 下午8:51
+ */
+namespace bengbeng\framework\components\plugins\alipay;
+
+class EncryptResponseData {
+
+	public $realContent;
+	public $returnContent;
+
+} 
