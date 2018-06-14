fc56106396416ede2eae67136eb4984ed0ea410a
hahastein <146119@qq.com>
2018-06-14 13:54:24 +0800

    更新

--- components/plugins/OutputPlugins.php
+++ components/plugins/OutputPlugins.php
@@ -15,6 +15,6 @@ class OutputPlugins extends Component{
         \Yii::$app->response->format = \yii\web\Response::FORMAT_HTML;
         $content = '<meta charset="UTF-8"><pre style="display: block;padding: 9.5px;margin: 40px 0px 10px 0px;font-size: 13px;line-height: 1.42857;color: #333;word-break: break-all;word-wrap: break-word;background-color: #F5F5F5;border: 1px solid #CCC;border-radius: 4px;">'.print_r($output_content,true).'</pre>';
         \Yii::$app->response->content = $content;
-        exit();
+//        exit();
     }
 }
