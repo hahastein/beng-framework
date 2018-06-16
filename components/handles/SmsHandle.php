cdad36668d7a040d796235013ab4b919745a39c4
hahastein <146119@qq.com>
2018-06-17 01:25:37 +0800

    update user handle

--- components/handles/SmsHandle.php
+++ components/handles/SmsHandle.php
@@ -70,7 +70,7 @@ class SmsHandle
                         $transaction->commit();
                         return [200, $message];
                     }else{
-                        \Yii::$app->B->outHtml($message);
+                        \Yii::$app->B->outHtml($message['msg']);
                         throw new Exception($message->msg);
                     }
                 });
