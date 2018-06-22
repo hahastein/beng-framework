9e07f5d64224275789f49406cc5870250b6f70d1
hahastein <146119@qq.com>
2018-06-22 19:43:30 +0800

    更新

---
+++ components/driver/upload/UpyunDriver.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by BengBengFramework.
+ * User: hahastein
+ * Date: 2018-06-22
+ * Time: 12:16
+ */
+
+namespace bengbeng\framework\components\driver\upload;
+
+class UpyunDriver{
+
+}
--- components/handles/UploadHandle.php
+++ components/handles/UploadHandle.php
@@ -17,11 +17,25 @@ class UploadHandle
     const UPLOAD_TYPE_LOCAL = 1;
     const UPLOAD_TYPE_UPYUN = 2;
 
-    private $uploadType;
+    private $config = array(
+        'mimes'         =>  array(), //允许上传的文件MiMe类型
+        'maxSize'       =>  0, //上传的文件大小限制 (0-不做限制)
+        'exts'          =>  array(), //允许上传的文件后缀
+        'autoSub'       =>  true, //自动子目录保存文件
+        'subName'       =>  array('date', 'Y-m-d'), //子目录创建方式，[0]-函数名，[1]-参数，多个参数使用数组
+        'rootPath'      =>  '../Uploads/', //保存根路径
+        'savePath'      =>  '', //保存路径
+        'saveName'      =>  array('uniqid', ''), //上传文件命名规则，[0]-函数名，[1]-参数，多个参数使用数组
+        'saveExt'       =>  '', //文件保存后缀，空则使用原后缀
+        'replace'       =>  false, //存在同名是否覆盖
+        'hash'          =>  true, //是否生成hash编码
+        'driver'        =>  '', // 文件上传驱动
+        'driverConfig'  =>  array(), // 上传驱动配置
+    );
 
-    public function __construct($type)
+    public function __construct($config)
     {
-        $this->uploadType = $type;
+        $this->config = array_merge($this->config, $config);
     }
 
     public function save(){
@@ -65,6 +79,10 @@ class UploadHandle
 
     }
 
+    private static function _files(){
+
+        $file  = $_FILES;
+    }
 
 //    public function __set($key, $value)
 //    {
