e0908593e219ccf7eed01d8d4fcf0d2dd6c79a44
hahastein <146119@qq.com>
2018-06-24 02:53:20 +0800

    update user handle

--- components/driver/upload/LocalDriver.php
+++ components/driver/upload/LocalDriver.php
@@ -79,6 +79,7 @@ class LocalDriver{
             return false;
         }
 
+        p($filename);
         /* 移动文件 */
         if (!move_uploaded_file($file['tmp_name'], $filename)) {
             $this->error = '文件上传保存错误！';
