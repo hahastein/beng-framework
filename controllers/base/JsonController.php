9e009788dd4d76033f2de7f6e3fbd5a90a558a21
hahastein <146119@qq.com>
2018-07-07 18:19:26 +0800

    更新

--- controllers/base/JsonController.php
+++ controllers/base/JsonController.php
@@ -21,7 +21,7 @@ class JsonController extends Controller{
                 'Origin' => ['*'],
                 'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'HEAD', 'OPTIONS'],
                 'Access-Control-Request-Headers' => ['*'],
-                'Access-Control-Allow-Credentials' => true,
+                'Access-Control-Allow-Credentials' => false,
                 'Access-Control-Max-Age' => 86400,
                 'Access-Control-Expose-Headers' => []
             ]
