e203b81fb87bfccbaf16d85a8df25b1d138bb7c7
hahastein <146119@qq.com>
2018-07-10 20:39:20 +0800

    更新

---
+++ components/handles/AddressHandle.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by BengBengFramework.
+ * User: hahastein
+ * Date: 2018-07-10
+ * Time: 19:00
+ */
+
+namespace bengbeng\framework\components\handles;
+
+
+class AddressHandle
+{
+
+}
