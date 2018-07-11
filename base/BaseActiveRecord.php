9788acf32ff00d687461e014bb653089ca5b4a3c
hahastein <146119@qq.com>
2018-07-12 01:16:14 +0800

    update user handle

---
+++ base/BaseActiveRecord.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hahastein
+ * Date: 2018/7/11
+ * Time: 21:09
+ */
+
+namespace bengbeng\framework\base;
+
+
+use yii\db\ActiveRecord;
+
+class BaseActiveRecord extends ActiveRecord
+{
+
+}
