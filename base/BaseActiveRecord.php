11ecfd4049dc42127c72955a44edfc5ebc3d105c
hahastein <146119@qq.com>
2018-07-31 00:11:11 +0800

    update user handle

--- base/BaseActiveRecord.php
+++ base/BaseActiveRecord.php
@@ -9,6 +9,7 @@
 namespace bengbeng\framework\base;
 
 use yii\data\Pagination;
+use yii\db\ActiveQuery;
 use yii\db\ActiveRecord;
 
 class BaseActiveRecord extends ActiveRecord
@@ -20,16 +21,33 @@ class BaseActiveRecord extends ActiveRecord
     private $dataCount = 0;
 
     public function info($where = false){
+        return self::dataOne(function (ActiveQuery $query) use($where){
+            $query->where($where);
+        });
+    }
+
+    /**
+     * 通用返回单条数据，如需要设定其他参数，请参看参数说明
+     * @param \Closure|null $callback 回调方法，返回ActiveQuery类型的$query
+     * @return array|null|ActiveRecord
+     */
+    public function dataOne(\Closure $callback = null){
         $query = self::find();
-        $query->where($where);
+        if($callback){
+            call_user_func($callback, $query);
+        }
         return $query->one();
     }
 
     /**
      * 通用返回数据集合，如需要设定其他参数，请参看参数说明
      * @param \Closure $callback 回调方法，返回ActiveQuery类型的$query
-     * 参照实例如下:
-     * (new Customer())->dataSet(function(\yii\db\ActiveQuery $query){
+     * 如需自定义设置翻页数量，请在params.php中设置Page参数的PageSize属性
+     * 调用参照实例如下:
+     * customer = new Customer();
+     * customer->pageSize = 30;        设置每页显示数量
+     * customer->validatePage = false; 设置最后不返回数据
+     * customer->dataSet(function(\yii\db\ActiveQuery $query){
      *      $query->with('custom');
      *      ...
      * });
