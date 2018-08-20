290bf4275e2da27a7cf596f60871a4a8c1235e5e
hahastein <146119@qq.com>
2018-08-20 11:10:46 +0800

    add 平台统计

--- components/handles/platform/StatisticsHandle.php
+++ components/handles/platform/StatisticsHandle.php
@@ -9,10 +9,12 @@
 namespace bengbeng\framework\components\handles\platform;
 
 
+use bengbeng\framework\base\data\ActiveOperate;
 use bengbeng\framework\base\Enum;
 use bengbeng\framework\models\PlatformStatisticsARModel;
 use yii\db\ActiveQuery;
 use yii\db\ActiveRecord;
+use yii\db\Expression;
 
 class StatisticsHandle
 {
@@ -33,14 +35,31 @@ class StatisticsHandle
     public function getStatistics($column){
         return array_filter($this->data, function ($item) use ($column){
             return $item['model_name'] == $column;
-        });
+        })[0];
 
-//        switch ($column){
-//            case Enum::PLATFORM_STATISTICS_ACCOUNT_AMOUNT:
-//                return '';
-//            default:
-//                return '';
-//        }
+    }
+
+    public function saveModel($column, $updateValue, $des){
+        return $this->_model->dataUpdate(function (ActiveOperate $operate) use($column,$updateValue,$des){
+            $operate->where(['model_name' => $column]);
+            switch ($column){
+                case Enum::PLATFORM_STATISTICS_ACCOUNT_AMOUNT:
+                    $operate->params([
+                        'model_int' => new Expression('model_int + '.$updateValue),
+                        'lasttime' => time(),
+                        'des' => empty($des)?'向平台增加金额：'.$updateValue.'元':$des
+                    ]);
+                    break;
+                default:
+                    $operate->params([
+                        'model_value' => $updateValue,
+                        'lasttime' => time(),
+//                        'des' => empty($des)?'向平台增加金额：'.$updateValue.'元':$des
+                    ]);
+                    break;
+            }
+
+        });
     }
 
 }
