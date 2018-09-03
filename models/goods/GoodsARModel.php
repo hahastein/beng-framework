01e0777d0261144546eb5b6597d45ddda3f4500c
hahastein <146119@qq.com>
2018-09-03 10:28:14 +0800

    update setting

---
+++ models/goods/GoodsARModel.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hahastein
+ * Date: 2018/9/2
+ * Time: 1:48
+ */
+
+namespace bengbeng\framework\models\goods;
+
+
+use bengbeng\framework\base\BaseActiveRecord;
+
+class GoodsARModel extends BaseActiveRecord
+{
+
+}
