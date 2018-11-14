e6407df76070f92fb0bf70d8725c89d864392b42
hahastein <146119@qq.com>
2018-11-14 21:12:44 +0800

    update menu

--- widgets/menu/StrongMenu.php
+++ widgets/menu/StrongMenu.php
@@ -56,7 +56,7 @@ class StrongMenu extends Widget
             $menuModel = new MenuARModel();
             $menuModel->showPage = false;
             $cache_data = $menuModel->dataSet(function (ActiveQuery $query){
-                $query->select(['menu_id', 'menu_name', 'menu_icon', 'module', 'controller', 'action', 'parent_id', 'order']);
+                $query->select(['menu_id', 'menu_name', 'menu_icon', 'module', 'controller', 'action', 'parent_id', 'order', 'initials']);
                 $query->where(['menu_type' => $this->type]);
                 $query->orderBy([
                     'parent_id' => SORT_ASC,
