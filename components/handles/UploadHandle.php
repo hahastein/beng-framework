5760e89b748fd8112f9a170220af591a704515c4
hahastein <146119@qq.com>
2019-04-22 00:03:35 +0800

    update upload tools

--- components/handles/UploadHandle.php
+++ components/handles/UploadHandle.php
@@ -42,7 +42,7 @@ class UploadHandle
             'savePath' => '', //保存路径
             'folderNameMode' => ['fun' => 'date', 'param' => 'Ymd'], //子目录创建方式，[0]-函数名，[1]-参数，多个参数使用数组
             'fileNameMode' => false, //上传文件命名规则，[0]-函数名，[1]-参数，多个参数使用数组
-            'saveExt' => '', //文件保存后缀，空则使用原后缀
+            'fileExt' => '', //文件保存后缀，空则使用原后缀
             'replace' => false, //存在同名是否覆盖
             'thumbnail' => false,
             'sdkConfig' => [
