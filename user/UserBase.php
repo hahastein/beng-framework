a36c8e1a4dd925c634a0a639362421e33adc57ef
hahastein <146119@qq.com>
2019-08-06 01:10:30 +0800

    update 20190805

--- user/UserBase.php
+++ user/UserBase.php
@@ -29,6 +29,9 @@ class UserBase
             return (new UserARModel())->findAllByUnionId($this->unionID);
         });
 
+
+        var_dump($userData);die;
+
         if(array_key_exists('user_id', $userData)){
             return $userData['user_id'];
         }
