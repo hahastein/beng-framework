69a4a3b75192c330a3f8fe734c34281c36b0b43e
hahastein <146119@qq.com>
2019-08-07 00:52:54 +0800

    update 20190805

--- user/UserUtil.php
+++ user/UserUtil.php
@@ -20,9 +20,6 @@ class UserUtil
         if($getNew){
             return CacheHandle::get($cacheName, function () use ($unionID){
                 $userModel = (new UserARModel())->findAllByUnionId($unionID);
-                var_dump($unionID);
-
-                var_dump($userModel);
                 if($userModel){
                     return $userModel->toArray();
                 }else{
