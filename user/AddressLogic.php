dacf60e7a94b530417b14fbd19e65bfd2e976ca3
hahastein <146119@qq.com>
2019-08-14 15:19:19 +0800

    update 20190805

--- user/AddressLogic.php
+++ user/AddressLogic.php
@@ -144,8 +144,8 @@ class AddressLogic extends UserBase
 
         $this->model->setAttributes($this->saveParams);
 
-var_dump($this->model);die;
         if($this->model->load($this->saveParams, '')){
+            var_dump($this->model);die;
 
 //            if(isset($this->params['is_default'])){
 //                $this->model->is_default = $this->saveParams['is_default'];
