0fedca8fa5565f65067f5a4d7ff52ac6ba9bede6
hahastein <146119@qq.com>
2019-08-16 17:11:59 +0800

    update 20190805

---
+++ system/SettingProperty.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace bengbeng\framework\system;
+
+
+class SettingProperty
+{
+
+    public $appName;
+    public $webName;
+    public $cateVersion;
+    public $cityVersion;
+
+    public function __construct($setting)
+    {
+        foreach ($setting as $value){
+            $key = $value['setting_name'];
+            if(isset($this->$key)){
+                $this->$key = $value['setting_value'];
+            }
+        }
+    }
+
+}
---
+++ system/System.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace bengbeng\framework\system;
+
+
+use bengbeng\framework\base\BaseModules;
+
+class System extends BaseModules
+{
+
+}
