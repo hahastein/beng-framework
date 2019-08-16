77f82d29b70f9e4f511ef85e71308d654adcb6e5
hahastein <146119@qq.com>
2019-08-16 18:22:44 +0800

    update 20190805

--- system/SettingProperty.php
+++ system/SettingProperty.php
@@ -14,16 +14,12 @@ class SettingProperty
 
     public function __construct($setting)
     {
-
         foreach ($setting as $value){
             $key = $value['setting_name'];
             if(property_exists($this, $key)){
                 $this->$key = $value['setting_value'];
             }
         }
-        var_dump($this);
-
-        die;
     }
 
 }
