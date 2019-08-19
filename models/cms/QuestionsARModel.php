f027ddbf7ae1709c56eb13e77f2d3c26bee76cb8
hahastein <146119@qq.com>
2019-08-19 16:45:41 +0800

    update 20190805

--- models/cms/QuestionsARModel.php
+++ models/cms/QuestionsARModel.php
@@ -86,7 +86,6 @@ class QuestionsARModel extends BaseActiveRecord
                 $this->with[] = 'images';
             }
 
-            var_dump($this->with);die;
             $query->with($this->with);
 
             $query->where(['status' => Enum::SYSTEM_STATUS_SUCCESS]);
