95680ed952e6a38e02f56540de5c2d1f1658a917
hahastein <146119@qq.com>
2019-08-25 19:45:44 +0800

    update 20190805

--- models/admin/AdminPermissionsARModel.php
+++ models/admin/AdminPermissionsARModel.php
@@ -6,6 +6,13 @@ namespace bengbeng\framework\models\admin;
 
 use bengbeng\framework\base\BaseActiveRecord;
 
+/**
+ * Class AdminPermissionsARModel
+ * @property integer $admin_id
+ * @property integer $permissions_id
+ * @property integer $gp_id
+ * @package bengbeng\framework\models\admin
+ */
 class AdminPermissionsARModel extends BaseActiveRecord
 {
     public static function tableName()
