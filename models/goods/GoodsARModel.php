be54689c05cdb31035bbede5e92bde2d4ea82793
hahastein <146119@qq.com>
2019-08-31 02:21:36 +0800

    update 20190805

--- models/goods/GoodsARModel.php
+++ models/goods/GoodsARModel.php
@@ -57,6 +57,12 @@ class GoodsARModel extends BaseActiveRecord
         ]);
     }
 
+    public function getImageDefault(){
+        return $this->hasOne(AttachmentARModel::className(),['object_id'=>'goods_id'])->where(['att_type' => Enum::MODULE_TYPE_GOODS, 'is_default' => 1])->select([
+            'object_id', 'obj_url', 'is_default'
+        ]);
+    }
+
     public function findStatusByAll(){
         return self::dataSet(function (ActiveQuery $query){
             $query->where([
