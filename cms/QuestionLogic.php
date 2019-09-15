90cedd056abe1d825e282cf0df2e0367286e3e85
hahastein <146119@qq.com>
2019-09-15 23:02:29 +0800

    update 20190805

--- cms/QuestionLogic.php
+++ cms/QuestionLogic.php
@@ -70,7 +70,6 @@ class QuestionLogic extends CmsBase
         foreach ($data['identify'] as $key => $identify){
             if($identify['user']){
                 $auth_info = json_decode($identify['user']['auth_info']);
-                $identify['doctor_name'] = $auth_info['doctorname'];
 
             }
             unset($data['identify'][$key]['user_id'],$data['identify'][$key]['question_id']);
