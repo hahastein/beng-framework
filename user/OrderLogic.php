b60efa289885e2b82c2edb1ace6ad3ae27f2099b
hahastein <146119@qq.com>
2019-11-16 17:25:32 +0800

    update 20190805

---
+++ user/OrderLogic.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace bengbeng\framework\user;
+
+use bengbeng\framework\models\order\OrdersARModel;
+use yii\db\ActiveQuery;
+
+class OrderLogic extends UserBase
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->moduleModel = new OrdersARModel();
+    }
+
+    /**
+     * 未发货订单总数
+     * @return int
+     */
+    public function unfilledCount(){
+        return OrdersARModel::find()->where([
+            'user_id' => $this->getUserID(),
+            'order_state' => 20,
+            'delete_state' => 0,
+            'refund_state' => 0
+        ])->count();
+    }
+}
