a964e5db5322a03d8bf7a49661eb8f140b2fafed
hahastein <146119@qq.com>
2019-12-24 11:35:23 +0800

    update 20191124

---
+++ components/handles/StructureHandle.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace bengbeng\framework\components\handles;
+
+/**
+ * 优化数据体结构处理
+ * Class StructureHandle
+ * @package bengbeng\framework\components\handles
+ */
+class StructureHandle
+{
+    public static function NicknameAndAvatar(&$user, $key = 'user'){
+
+        if(!isset($user[$key])){
+            $user[$key]['nickname'] = $user['nickname'];
+            $user[$key]['avatar_head'] = $user['avatar_head'];
+        }else{
+            unset($user[$key]['user_id']);
+        }
+
+    }
+
+    /**
+     * 性别相互转换
+     * @param $sex
+     * @return mixed
+     */
+    public static function Sex($sex){
+        $sex_team = [
+            0 => '无',
+            1 => '男',
+            2 => '女'
+        ];
+
+        return $sex_team[$sex];
+    }
+}
