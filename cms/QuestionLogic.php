df3f78a01deddd59e0944f406d7fe2a54a3bf139
hahastein <146119@qq.com>
2020-01-03 14:53:30 +0800

    update 20191124

--- cms/QuestionLogic.php
+++ cms/QuestionLogic.php
@@ -340,12 +340,14 @@ class QuestionLogic extends CmsBase
             $is_doctor = $this->getUser()->isAuth;
 
             if($identity && $is_doctor){
+                //获取医生的DoctorID
+                $celebrity = CelebrityARModel::findOne(['celebrity_id' => $this->getUserID()]);
 
-                if($questionModel->group_id){
-                    if($questionModel->group_id != $this->getUser()){
+                if($questionModel->celebrity_id){
+                    if($questionModel->celebrity_id != $celebrity->celebrity_id){
                         throw new Exception('此问题已被其他医生抢答');
                     }
-                    $group_id = $questionModel->group_id;
+                    $group_id = $questionModel->celebrity_id;
                 }else{
                     $group_id = $this->getUser();
                 }
